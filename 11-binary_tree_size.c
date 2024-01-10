88d2eefda2fc0c844af66ac184283740936b8177
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-01-10 18:20:03 -0400

    Betty correction for task 11 files

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,12 +9,12 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0;
+	size_t size = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
-    return (size);
+	return (size);
 }
